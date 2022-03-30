adb455919f8b22b3aa68335b90baf76730ac87d1
MUKILTEO\1621728 <1621728@mukilteo.wednet.edu>
2022-03-30 12:11:00 -0700

    Got the "stuff" Snakes.

---
+++ Assets/Scripts/UnitClick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class UnitClick : MonoBehaviour
+{
+
+    private Camera myCam;
+
+    public LayerMask clickable;
+    public LayerMask ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/UnitSelections.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelections : MonoBehaviour
+{
+    public List<GameObject> unitList = new List<GameObject>();
+    public List<GameObject> unitsSelected = new List<GameObject>();
+
+    private static UnitSelections _instance;
+    public static UnitSelections Instance { get { return _instance; } }
+
+
+    void Awake()
+    {
+        //If instance exists already and isn't this one
+        if(_instance != null && _instance != this)
+        {
+            //we destroy this
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            //make this the instance
+            _instance = this;
+        }
+    }
+
+    public void ClickSelect(GameObject unitToAdd)
+    {
+
+    }
+
+    public void ShiftClickSelect(GameObject unitToAdd)
+    {
+
+    }
+
+    public void DragSelect(GameObject unitToAdd)
+    {
+
+    }
+
+    public void DeselectAll()
+    {
+
+    }
+
+    public void Deselect(GameObject unitToDeselect)
+    {
+
+    }
+}
